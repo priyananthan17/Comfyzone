0a9d00625cefb90ca955b82461185981a67a199f
SimbuM <simbum62@gmail.com>
2024-06-29 11:01:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import HomePage from './Pages/HomePage';
+import New from './Pages/New';
+import Snow from './Pages/Snow';
+import Rain from './Pages/Rain';
+import ShopAll from './Pages/ShopAll';
+import Summer from './Pages/Summer';
+import Details from './Pages/Details';
+import {Route,Routes} from 'react-router-dom'
+import Footer from './Components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/new' element={<New/>}/>
+      <Route path='/rain' element={<Rain/>}/>
+      <Route path='/summer' element={<Summer/>}/>
+      <Route path='/snow' element={<Snow/>}/>
+      <Route path='/details/:id' element={<Details/>}/>
+      <Route path='/shopall' element={<ShopAll/>}/>
+    </Routes>
+    
+      <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 import reportWebVitals from './reportWebVitals';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { BrowserRouter } from 'react-router-dom';
+
+        
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+       <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
